3dab94efa8ae440ff0f450934f2dc6e7bac7d0cb
Sylph1de <Francodavidpietrokovsky@gmail.com>
2023-03-04 14:45:30 -0300

    ?

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 mkdir -p ~/.streamlit/
 
-echo "\
+echo '\
 [runner]\n\
 fastReruns = true\n\
 \n\
 [theme]\n\
-base=dark\n\
-primaryColor = violet\n\
+base="dark"\n\
+primaryColor = "violet"\n\
 \n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-" > ~/.streamlit/config.toml
+' > ~/.streamlit/config.toml
