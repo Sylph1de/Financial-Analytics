768c28f2b052c57cafdf64aafaa24d5b58b33abc
Sylph1de <Francodavidpietrokovsky@gmail.com>
2023-03-04 14:21:44 -0300

    added setup.sh and Procfile

---
+++ setup.sh
@@ -0,0 +1,14 @@
+echo "\
+[runner]\n\
+fastReruns = true\n\
+\n\
+[theme]\n\
+base="dark"\n\
+primaryColor = "rgb(178, 126, 255)"\n\
+\n\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
