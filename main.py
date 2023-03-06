314be1b3ee95668b2537d3ad466f42e55c15d983
Sylph1de <Francodavidpietrokovsky@gmail.com>
2023-03-06 20:53:35 -0300

    added try catch for empty sheets, added pin system

--- main.py
+++ main.py
@@ -17,52 +17,76 @@ load_dotenv()
 filterwarnings('ignore')
 
 # ? CONFIG
+title = 'Finanzas'
 st.set_page_config(
-    page_title='Finanzas (Franco) - DEV' if getenv('DEV') else 'Finanzas (Franco)',
+    page_title=title + ' - DEV' if getenv('DEV') else title,
     page_icon=':money_with_wings:',
     layout='wide',
     menu_items={
         "Get help": None,
         "Report a Bug": None,
-        "About": 'Sheet:\n'+getenv('SHEET_URL'),
     },
 )
-sheet = get_sheet()
-records = sheet.get_all_records()
-df = pd.DataFrame(records)
-# ? TRANSFORMATION
-df.Gasto = df.Gasto.astype(float).apply(lambda x: round(x, 2))
-df.Ingreso = df.Ingreso.astype(float).apply(lambda x: round(x, 2))
+st.title(':violet[Finanzas]')
+with st.columns(5)[0]:
+    st.session_state.pin = st.text_input('Pin', placeholder='PIN', max_chars=4, label_visibility='collapsed', type='password')
+if st.session_state.pin:
+    sheet = get_sheet()
+    records = sheet.get_all_records()
+    df = pd.DataFrame(records)
 
-df.Fecha = pd.to_datetime(df.Fecha, format='%d/%m/%Y')
+    # ? TRANSFORMATION
+    try:
+        df.Gasto = df.Gasto.astype(float).apply(lambda x: round(x, 2))
+        df.Ingreso = df.Ingreso.astype(float).apply(lambda x: round(x, 2))
+        df.Fecha = pd.to_datetime(df.Fecha, format='%d/%m/%Y')
+    except Exception:
+        pass
 
-# ? LAYOUT
-analytics, full_view, add_data = st.tabs(['Análisis', 'Vista previa', 'Agregar registro'])
+    # ? LAYOUT
+    analytics, full_view, add_data = st.tabs(['Análisis', 'Vista previa', 'Agregar registro'])
 
-with analytics:
+    with analytics:
+        try:
+            with st.expander('**General**', expanded=True):
+                get_general(df)
 
-    with st.expander('**General**', expanded=True):
-        get_general(df)
+            columns = st.columns(3)
 
-    columns = st.columns(3)
+            with columns[0]:
+                with st.expander('**Gastos hormiga**', expanded=True):
+                    get_ants(df)
 
-    with columns[0]:
-        with st.expander('**Gastos hormiga**', expanded=True):
-            get_ants(df)
+            with columns[1]:
+                with st.expander('**Gastos**', expanded=True):
+                    get_spending(df)
 
-    with columns[1]:
-        with st.expander('**Gastos**', expanded=True):
-            get_spending(df)
+            with columns[2]:
+                with st.expander('**Ingresos**', expanded=True):
+                    get_income(df)
+        except Exception:
+            st.info('Aún no tenés los suficientes registros para poder hacer un análisis', icon='😔')
 
-    with columns[2]:
-        with st.expander('**Ingresos**', expanded=True):
-            get_income(df)
+    with full_view:
+        get_full_view(df)
 
-with full_view:
-    get_full_view(df)
+    with add_data:
+        columns = st.columns([1, 3, 1])
 
-with add_data:
-    columns = st.columns([1, 3, 1])
+        with columns[1]:
+            get_add_data()
 
-    with columns[1]:
-        get_add_data()
+    # with remove_data:
+    #     columns = st.columns([1, 1, 4, 2, 1.5, 1.5, 4])
+    #     for i, col in enumerate(df.columns, start=1):
+    #         with columns[i]:
+    #             st.markdown(col)
+    #     with columns[0]:
+    #         st.markdown('Acción')
+    #     for i, element in enumerate(df.to_dict('records')):
+    #         with columns[0]:
+    #             st.button('Delete')
+    #         with columns[1]:
+    #             st.markdown(element['Fecha'].strftime('%d-%b-%Y'))
+    #     with columns[-1]:
+    #         st.button('Eliminar registros seleccionados')
--- resources/utils.py
+++ resources/utils.py
@@ -1,6 +1,7 @@
 from os import environ, getenv
 
 import gspread
+import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
 
 
@@ -9,5 +10,14 @@ def get_sheet():
   gcp_data = {key.replace('PROJECT_GCP_', '').lower(): environ.get(key) for key in gcp_keys}
   creds = ServiceAccountCredentials.from_json_keyfile_dict(gcp_data)
   client = gspread.authorize(creds)
-  sheet = client.open_by_url(getenv('SHEET_URL')).sheet1
+  pin = st.session_state.pin
+  if pin == getenv('LAURA_PIN'):
+    sheet_url = getenv('LAURA_SHEET_URL')
+  elif pin == getenv('FRANCO_PIN'):
+    sheet_url = getenv('FRANCO_SHEET_URL')
+  elif pin == getenv('WALTER_PIN'):
+    sheet_url = getenv('WALTER_SHEET_URL')
+  elif pin == getenv('TEST_PIN'):
+    sheet_url = getenv('TEST_SHEET_URL')
+  sheet = client.open_by_url(sheet_url).sheet1
   return sheet
