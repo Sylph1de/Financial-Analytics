1cd09fb5f11818582348f4db4ff0d52c2d73c5ad
Sylph1de <Francodavidpietrokovsky@gmail.com>
2023-03-06 00:00:32 -0300

    fixed timezone

--- resources/data/dates.py
+++ resources/data/dates.py
@@ -1,9 +1,14 @@
 from datetime import datetime, timedelta
 
+from pytz import timezone
+
 
 def get_dates():
+  tz = timezone('America/Buenos_Aires')
+
   class Dates:
-    current_date = datetime.now()
+
+    current_date = datetime.now(tz)
     previous_date = current_date - timedelta(1)
     current_year = current_date.year
     previous_year = current_year - 1
